1a05dac1c7fc003b1fcb3e39e8a5110e48cbed43
Rathapol <Rathapol@cn333.com>
2023-07-08 21:43:47 +0700

    edit http response

--- todolist/views.py
+++ todolist/views.py
@@ -16,26 +16,30 @@ def post(request):
     serializer = TodoListSerializer(data=request.data)  
     if serializer.is_valid():  
         serializer.save()  
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)  
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_201_CREATED)  
     else:  
         return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 @api_view(["GET"])
 def get(request):  
     result = Todo.objects.all()  
     serializers = TodoListSerializer(result,many=True)  
-    return Response({'status': 'success', "data":serializers.data}, status=200)
+    return Response({'status': 'success', "data":serializers.data}, status=status.HTTP_200_OK)
 
 @api_view(['PUT'])
 def update(request, id):
-    newData = Todo.objects.get(id=id)
-    serializers = TodoListSerializer(instance=newData, data=request.data,partial=True)
+    
+    try:
+        newData = Todo.objects.get(id=id)
+        serializers = TodoListSerializer(instance=newData, data=request.data,partial=True)
+    except:
+        return Response("id is not in databases",status=status.HTTP_400_BAD_REQUEST)
+
 
     if serializers.is_valid():
         serializers.save()
-        return Response({"status": "success", "data": serializer.data})
+        return Response(serializers.data,status=status.HTTP_200_OK)
 
-    else:
-        return Response({"status": "error", "data": serializer.errors})
+
 
 
 @api_view(['DELETE'])
@@ -43,7 +47,7 @@ def delete(request, id):
 
     deleteData = get_object_or_404(Todo,id=id)
     deleteData.delete()
-    return Response({"status": "success", "data": "Record Deleted"}) 
+    return Response({"status": "success", "data": "Record Deleted"},status=status.HTTP_200_OK) 
 
 
 
